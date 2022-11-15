3d230edd7b4a4b9b2c7941ebd6e6020a40f74c70
CJAOliveira <85760730+CJAOliveira@users.noreply.github.com>
2022-11-15 17:45:43 -0400

    Update newDagTest.py

--- dags/newDagTest.py
+++ dags/newDagTest.py
@@ -1,4 +1,4 @@
-from codPython.coletaDados_2 import main
+from "s3://trabalhofinal285785619488/args/coletaDados_2" import main
 # from codPython.transformaDadosSpark import process_start_1
 # from codPython.indicador_final import process_start_2
 from airflow import DAG
