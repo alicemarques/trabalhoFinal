51d5c03f9770cabdcce95b73b1d5885ba2f305d7
CJAOliveira <85760730+CJAOliveira@users.noreply.github.com>
2022-11-15 16:15:55 -0400

    Update newDagTest.py

--- dags/newDagTest.py
+++ dags/newDagTest.py
@@ -1,4 +1,4 @@
-# rom codPython.coletaDados_2 import main
+from codPython.coletaDados_2 import main
 # from codPython.transformaDadosSpark import process_start_1
 # from codPython.indicador_final import process_start_2
 from airflow import DAG
